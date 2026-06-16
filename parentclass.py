3c9df4298d6cc6f61c88489bdd4809a08b2115c5
prashichhayanpokharel7-ui <prashichhayanpokharel7@gmail.com>
2026-06-16 19:02:17 +0545

    Add files via upload

---
+++ parentclass.py
@@ -0,0 +1,19 @@
+#parent
+# class person:
+#     name = "Ram"
+#     age = 20 
+#     address = 'Lalitpur'
+# class staff(person):
+#     emp_type = 'Full Time'
+#     job = 'Front End Developer'
+#     skills = 'React Js,SQL,HTML'
+
+# s = staff()
+
+# # def print_details(self):
+# print(f"Name: {s.name}")
+# print(f"age: {s.age}")
+# print(f"address: {s.address}")
+# print(f"Employee type: {s.emp_type}")
+# print(f"Job: {s.job}")
+# print(f"Skills: {s.skills}") 
